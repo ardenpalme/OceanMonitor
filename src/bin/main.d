c76d625b90f11b106e069ce26aa3624168035aa8
ardendiak <ardendiak@gmail.com>
2022-07-10 20:01:08 -0700

    Initial commit libopencm3 USART example

---
+++ src/bin/main.d
@@ -0,0 +1,20 @@
+bin/main.o: main.c ../libopencm3/include/libopencm3/stm32/rcc.h \
+ ../libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ ../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/l1/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/l1/rcc.h \
+ ../libopencm3/include/libopencm3/stm32/pwr.h \
+ ../libopencm3/include/libopencm3/stm32/l1/pwr.h \
+ ../libopencm3/include/libopencm3/stm32/common/pwr_common_v2.h \
+ ../libopencm3/include/libopencm3/stm32/common/pwr_common_v1.h \
+ ../libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ ../libopencm3/include/libopencm3/stm32/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/l1/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ ../libopencm3/include/libopencm3/stm32/usart.h \
+ ../libopencm3/include/libopencm3/stm32/l1/usart.h \
+ ../libopencm3/include/libopencm3/stm32/common/usart_common_f124.h \
+ ../libopencm3/include/libopencm3/stm32/common/usart_common_all.h
---
+++ src/bin/my-project.d
@@ -0,0 +1,3 @@
+bin/my-project.o: my-project.c ../my-common-code/api.h \
+ ../my-common-code/api-asm.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h
