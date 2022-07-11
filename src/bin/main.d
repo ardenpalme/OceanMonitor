ef34d128cb658483189e4d2a14b3767f9ac0e5bc
ardendiak <ardendiak@gmail.com>
2022-07-11 13:59:00 -0700

    NMEA sentence parsing

--- src/bin/main.d
+++ src/bin/main.d
@@ -17,4 +17,15 @@ bin/main.o: main.c ../libopencm3/include/libopencm3/stm32/rcc.h \
  ../libopencm3/include/libopencm3/stm32/usart.h \
  ../libopencm3/include/libopencm3/stm32/l1/usart.h \
  ../libopencm3/include/libopencm3/stm32/common/usart_common_f124.h \
- ../libopencm3/include/libopencm3/stm32/common/usart_common_all.h
+ ../libopencm3/include/libopencm3/stm32/common/usart_common_all.h \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/newlib.h \
+ /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/stdlib.h /usr/include/newlib/string.h \
+ /usr/include/newlib/sys/string.h
--- src/main.c
+++ src/main.c
@@ -1,26 +1,10 @@
-/*
- * This file is part of the libopencm3 project.
- *
- * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
- * Copyright (C) 2012 Karl Palsson <karlp@tweak.net.au>
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NMEA_LEN 74
 
 static void clock_setup(void)
 {
@@ -29,56 +13,103 @@ static void clock_setup(void)
 	rcc_periph_clock_enable(RCC_GPIOA);
 	rcc_periph_clock_enable(RCC_GPIOB);
 
-	/* Enable clocks for USART2. */
+	/* Enable clocks for USART */
 	rcc_periph_clock_enable(RCC_USART2);
+	rcc_periph_clock_enable(RCC_USART1);
 }
 
-static void usart_setup(void)
+static void usart_setup(uint32_t usart, uint32_t baud_rate, uint32_t mode)
 {
 	/* Setup USART2 parameters. */
-	usart_set_baudrate(USART2, 115200);
-	usart_set_databits(USART2, 8);
-	usart_set_stopbits(USART2, USART_STOPBITS_1);
-	usart_set_mode(USART2, USART_MODE_TX);
-	usart_set_parity(USART2, USART_PARITY_NONE);
-	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+	usart_set_baudrate(usart, baud_rate);
+	usart_set_databits(usart, 8);
+	usart_set_stopbits(usart, USART_STOPBITS_1);
+	usart_set_mode(usart, mode);
+	usart_set_parity(usart, USART_PARITY_NONE);
+	usart_set_flow_control(usart, USART_FLOWCONTROL_NONE);
 
-	/* Finally enable the USART. */
-	usart_enable(USART2);
+	usart_enable(usart);
 }
 
 static void gpio_setup(void)
 {
-	/* Setup GPIO pin GPIO7 on GPIO port B for Green LED. */
+	/* Setup GPIO pin for Green LED. */
 	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
 
 	/* Setup GPIO pins for USART2 transmit. */
 	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
-
-	/* Setup USART2 TX pin as alternate function. */
 	gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
+
+	/* Setup GPIO pins for USART1 transmit. */
+	gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO6);
+	gpio_set_af(GPIOB, GPIO_AF7, GPIO6);
+
+	/* Setup GPIO pins for USART1 recieve. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO10);
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO10);
 }
+
+static void send_msg(uint32_t usart, char *msg, int msg_len)
+{
+    int i;
+    for (i=0; i<msg_len; i++) {
+        if(msg[i] != 0)
+            usart_send_blocking(usart, (uint16_t)msg[i]);
+    }
+}
+
+static void recv_msg(uint32_t usart, char *msg, int msg_len)
+{
+    int i;
+    uint16_t c;
+    for (i=0; i<msg_len; i++) {
+        c = usart_recv_blocking(usart);
+        msg[i] = (char)c;
+        if(c == '\n')
+            break;
+    }
+}
+
 
 int main(void)
 {
-	int i, j = 0, c = 0;
+	int i;
+    char msg[NMEA_LEN];
 
 	clock_setup();
 	gpio_setup();
-	usart_setup();
+	usart_setup(USART2, 115200, USART_MODE_TX);
+	usart_setup(USART1, 9600, USART_MODE_TX_RX);
 
-	/* Blink the LED (PD12) on the board with every transmitted byte. */
 	while (1) {
-		gpio_toggle(GPIOA, GPIO5); /* LED on/off */
-		usart_send_blocking(USART2, c + '0'); /* USART2: Send byte. */
-		c = (c == 9) ? 0 : c + 1; /* Increment c. */
-		if ((j++ % 80) == 0) { /* Newline after line full. */
-			usart_send_blocking(USART2, '\r');
-			usart_send_blocking(USART2, '\n');
-		}
-		for (i = 0; i < 100000; i++) { /* Wait a bit. */
-			__asm__("NOP");
-		}
+        recv_msg(USART1, msg, NMEA_LEN);
+		gpio_toggle(GPIOA, GPIO5);
+
+        // print and parse NMEA sentence
+        if(!strncmp(msg,"$GPGGA", 6)) {
+            //send_msg(USART2, msg, NMEA_LEN);
+            //usart_send_blocking(USART2, '\n');
+
+            i=0;
+            char *tok = strtok(msg, ",");
+            while(tok != NULL){
+                if(i > 1 && i <= 5) {
+                    send_msg(USART2, tok, strlen(tok));
+                    if(i % 2 != 0){
+                        if(i==5)
+                            usart_send_blocking(USART2, '\n');
+                        else
+                            usart_send_blocking(USART2, ',');
+                    }
+                }
+
+                tok = strtok(NULL, ",");
+                i++;
+            }
+        }else if(!strncmp(msg,"$GPGLL", 6)) {
+            send_msg(USART2, msg, NMEA_LEN);
+            usart_send_blocking(USART2, '\n');
+        }
 	}
 
 	return 0;
